f3b6fa32549e0e271a0ad21110533c47f0df8289
Kamor <kamor1970@gmx.de>
2022-01-03 20:00:42 +0100

    Create testpotions.lua

---
+++ maps/dev/testpotions.lua
@@ -0,0 +1,33 @@
+local pl = event.activator
+
+local potionreduce = pl:CreateObjectInsideEx("potion_reduce", game.IDENTIFIED, 100)
+	
+local potionh1 = pl:CreateObjectInsideEx("potion_minor_healing", game.IDENTIFIED, 1)
+local potionh2 = pl:CreateObjectInsideEx("potion_small_healing", game.IDENTIFIED, 1)
+local potionh3 = pl:CreateObjectInsideEx("potion_medium_healing", game.IDENTIFIED, 1)
+local potionh4 = pl:CreateObjectInsideEx("potion_strong_healing", game.IDENTIFIED, 1)
+local potionh5 = pl:CreateObjectInsideEx("potion_greater_healing", game.IDENTIFIED, 1)
+local potionh6 = pl:CreateObjectInsideEx("potion_superior_healing", game.IDENTIFIED, 1)
+		
+local potionm1 = pl:CreateObjectInsideEx("potion_minor_mana", game.IDENTIFIED, 1)
+local potionm2 = pl:CreateObjectInsideEx("potion_small_mana", game.IDENTIFIED, 1)
+local potionm3 = pl:CreateObjectInsideEx("potion_medium_mana", game.IDENTIFIED, 1)
+local potionm4 = pl:CreateObjectInsideEx("potion_strong_mana", game.IDENTIFIED, 1)
+local potionm5 = pl:CreateObjectInsideEx("potion_greater_mana", game.IDENTIFIED, 1)
+local potionm6 = pl:CreateObjectInsideEx("potion_superior_mana", game.IDENTIFIED, 1)
+	
+local potiong1 = pl:CreateObjectInsideEx("potion_minor_grace", game.IDENTIFIED, 1)
+local potiong2 = pl:CreateObjectInsideEx("potion_small_grace", game.IDENTIFIED, 1)
+local potiong3 = pl:CreateObjectInsideEx("potion_medium_grace", game.IDENTIFIED, 1)
+local potiong4 = pl:CreateObjectInsideEx("potion_strong_grace", game.IDENTIFIED, 1)
+local potiong5 = pl:CreateObjectInsideEx("potion_greater_grace", game.IDENTIFIED, 1)
+local potiong6 = pl:CreateObjectInsideEx("potion_superior_grace", game.IDENTIFIED, 1)
+
+local potionmh = pl:CreateObjectInsideEx("potion_half_mana", game.IDENTIFIED, 1)
+local potionmf = pl:CreateObjectInsideEx("potion_full_mana", game.IDENTIFIED, 1)
+			
+local potiongh = pl:CreateObjectInsideEx("potion_half_grace", game.IDENTIFIED, 1)
+local potiongf = pl:CreateObjectInsideEx("potion_full_grace", game.IDENTIFIED, 1)
+			
+local potionrh = pl:CreateObjectInsideEx("potion_half_restoration", game.IDENTIFIED, 1)
+local potionrf = pl:CreateObjectInsideEx("potion_full_restoration", game.IDENTIFIED, 1)
