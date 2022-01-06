8b14c4f740c4778f99b8d5525a2f58afb2ec127f
Kamor <kamor1970@gmx.de>
2022-01-06 20:25:34 +0100

    Add files via upload
    upgraded ranged_weapon_skill and small cleaning in melee_weapon_skill

--- maps/lua/helper/ranged_weapon_skill.lua
+++ maps/lua/helper/ranged_weapon_skill.lua
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------------
--- melee_weapon_skill.lua | Helper
+-- ranged_weapon_skill.lua | Helper
 --
--- Figure out which weapon skill the player has.
+-- Figure out which bow skill the player has.
 -- improved logic, first check for an equipped weapon and take this skill
 -- with no equipped weapon, search skills, compare the exp, if equal choose by skill index
 -------------------------------------------------------------------------------
@@ -9,9 +9,9 @@
 -- All helpers require a script context, so ensure the Utility is loaded.
 -- example to use this script from outside
 -- require("script_context")
--- require("helper/melee_weapon_skill")
--- local mws = ScriptContext:GetFunction("melee_weapon_skill")
--- local obj, mode = mws(player)
+-- require("helper/ranged_weapon_skill")
+-- local rws = ScriptContext:GetFunction("ranged_weapon_skill")
+-- local obj, mode = rws(player)
 ---------------------------------------
 require("script_context")
 
@@ -23,19 +23,18 @@ local function f(player)
         "Arg #1 must be player GameObject!")
 	
 	local obj, mode	-- if we find skill, we put in in here
-	-- check first if player has readied a weapon	
-	local weapon = player:GetEquipment(game.EQUIP_WEAPON1)
-	if weapon ~= nil then
-		-- modula since Lua 5.2 we are under 5.2?
-		local subtype = weapon.subtype - math.floor(weapon.subtype/4)*4
-		if subtype==game.WEAP_1H_IMPACT then
-			obj, mode = player:GetSkill("Impact Weapons")
-		elseif subtype==game.WEAP_1H_SLASH then
-			obj, mode = player:GetSkill("Slash Weapons")
-		elseif subtype==game.WEAP_1H_PIERCE then	
-			obj, mode = player:GetSkill("Pierce Weapons")
-		elseif subtype==game.WEAP_1H_CLEAVE then
-			obj, mode = player:GetSkill("Cleave Weapons")
+	-- check first if player has readied a bow	
+	local bow = player:GetEquipment(game.EQUIP_BOW)
+	if bow ~= nil then
+		if bow.subtype==game.RANGE_WEAP_BOW then
+			obj, mode = player:GetSkill("Bow Archery")
+		elseif bow.subtype==game.RANGE_WEAP_XBOWS then
+			obj, mode = player:GetSkill("Crossbow Archery")
+		elseif bow.subtype==game.RANGE_WEAP_SLINGS then	
+			obj, mode = player:GetSkill("Sling Archery")
+		elseif bow.subtype==game.RANGE_WEAP_FIREARMS then	
+			--obj, mode = player:GetSkill("xxx")
+			error(player:GetName() .. " range firearms archery not implemented!")			
 		end
 		
 		if obj then	
@@ -43,11 +42,11 @@ local function f(player)
 		end
 	end
 	
-	-- no weapon or no valid weapon skill found? we search skills now
+	-- no bow or no valid bow skill found? we search skills now
 	-- and compare the exp, if exp is equal it depends on the skill index
-	local sname = { "Cleave", "Impact", "Pierce", "Slash" }
+    local sname = { "Bow", "Crossbow", "Sling" }
     for _, v in ipairs(sname) do
-        local temp_obj, temp_mode = player:GetSkill(v .. " Weapons")
+        local temp_obj, temp_mode = player:GetSkill(v .. " Archery")
         if temp_obj ~= nil then
 			if obj == nil or temp_obj.experience>obj.experience then
 				obj=temp_obj
@@ -59,12 +58,12 @@ local function f(player)
 	if obj then
 	  return obj, mode -- return highest skill
 	end
-
-    error(player:GetName() .. " has no wskill!")
+	
+	error(player:GetName() .. " has no wskill!")
 end
 
 ---------------------------------------
 -- Add the function to the script context under the key of the Helper script
 -- name.
 ---------------------------------------
-ScriptContext:AddFunction("melee_weapon_skill", f)
+ScriptContext:AddFunction("ranged_weapon_skill", f)
