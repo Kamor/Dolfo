ef2ed0abe44bc5f09f855954739d2c680992af36
Kamor <kamor1970@gmx.de>
2022-01-13 18:07:31 +0100

    Add files via upload
    improved prototype of trader.lua, can adjust nrof and level from 1 to 9

--- maps/scripts/trader.lua
+++ maps/scripts/trader.lua
@@ -1,17 +1,55 @@
 require("topic_list")
 require("interface_builder")
 
+local MAX_NROF = 1000
+local MAX_LEVEL = 110
+
 local pl = event.activator
 local me = event.me
 
 local ib = InterfaceBuilder()
 ib:SetHeader(me, me.name)
 
+-- npc_shop can store 2 values in player data, nrof to adjust the amount to buy, level to adjust the level to buy
+local ds = DataStore("npc_shop", pl)
+
+local nrof = ds:Get("nrof")
+if nrof == nil or nrof<1 then
+	nrof = 1
+	ds:Set("nrof", nrof)
+	-- ds:WasChanged() -- looks like this is not needed
+elseif nrof>MAX_NROF then
+	nrof = MAX_NROF
+	ds:Set("nrof", nrof)
+	-- ds:WasChanged() -- looks like this is not needed
+end
+local level = ds:Get("level")
+if level == nil or level<1 then
+	level = 1
+	ds:Set("level", level)
+	-- ds:WasChanged() -- looks like this is not needed
+elseif level>MAX_LEVEL then
+	level = MAX_LEVEL
+	ds:Set("level", level)
+	-- ds:WasChanged() -- looks like this is not needed
+end
+
+--level = 5
+--ds:Set("level", level) -- test adjustment
+
+--nrof = 5
+--ds:Set("nrof", nrof) -- test adjustment
+
+--pl:Write(nrof, game.COLOR_YELLOW)
+--pl:Write(level, game.COLOR_YELLOW)
+
 -- these must be the arch names, using wrong archnames crashes the player
 -- some generic arches get their title from hardcode, so we can't read title
 --> for this arches we can overwrite title here
 local for_sale =
-{   {arch = "scroll_min_heal", title = "of minor healing"},
+{
+	{arch = "drink_generic"},
+	{arch = "scroll_min_heal", title = "of minor healing"},
     {arch = "scroll_firestorm", title = "of firestorm"},
 	--"scroll_firestorm",
 	--"scroll_icestorm",
@@ -30,9 +68,9 @@ local for_sale =
 
 local function topicDefault()
     ib:SetTitle("Trader")
-	ib:SetMsg("\n\nYou have " .. pl:ShowCost(pl:GetMoney()) .. "\n ")
-	
-	for what, data in for_sale do
+	ib:SetMsg("You have " .. pl:ShowCost(pl:GetMoney()) .. ".")
+	ib:AddMsg(" You carry " .. pl.carrying .. " (" .. pl:GetPlayerWeightLimit() ..") kg.") 
+	for index, data in for_sale do
 	  local ob = pl:CreateObject(data.arch, game.IDENTIFIED, 1) -- create 1 identified obj
 	  
 	  -- what happens with object when we don't use it? will it be removed from memory or must we handle this? <--------------------------
@@ -41,28 +79,104 @@ local function topicDefault()
 		if data.title ~= nil then
 			ob.title = data.title
 		end
+		-- if we don't overwrite empty ob.title comes with ob.title=="", so no need to check there for nil
+	
+		-- make a rule for level adjustment
+		-- 0 -> is always no level on this item
+		-- 1 -> we must check, if there are wrong arch definitions, like my new food potions with level 1
+		-- we can also go for spellcheck, all spell -1 have level 0 ? TODO
+		if ob.level > 0 then
+		   ob.level = level -- level comes now from datastore
+		   ob.title = ob.title .. " (lvl ".. ob.level ..")"
+		end
+		-- pl:Write(ob.level, game.COLOR_YELLOW)
 	  
 		local icon = ob:GetInvFace()
-		ib:AddSelect("Buy  1 " .. ob.name .. " " .. ob.title, "buy " .. what , icon , pl:ShowCost(ob.value) .. "index:" .. what)
+		if icon == nil then
+			icon = ob:GetFace()
+		end
+		if icon == nil then
+			icon = "bug.101"
+		end
+
+		local value= ob.value*(ob.level+1)*nrof 
+		ib:AddSelect(	"Buy " .. nrof .. " " .. ob.name .. " " .. ob.title,
+						"buy " .. nrof .. " " .. index .. " " .. ob.level,
+						icon ,
+						pl:ShowCost(value) .. " - " .. ob.weight*nrof .. " kg",
+						nrof	) 
 	  end	  
 	end
+	
+	ib:AddMsg(" ^nrof 1^ ^nrof 5^ ^level 1^ ^level 5^ ")
+	
     pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
 end
 
-local function topicBuy(index)
+local function topicBuy(nrof, index, level)
 	-- pl:Write("Yeah!", game.COLOR_YELLOW)
 	if index~=nil then
-		local i= tonumber(index)
-		local text1 = "Index : " .. i
+		local i = tonumber(index)
+		--local text1 = "Index : " .. i
 		--pl:Write(text1, game.COLOR_YELLOW)
 		if i>0 then -- todo max array cap
 			-- pl:Write(for_sale[i].arch, game.COLOR_YELLOW)
 			
-			local ob = pl:CreateObject(for_sale[i].arch, game.IDENTIFIED, 1) -- create 1 identified obj
+			-- nrof is nil or string, what happens on nrof = ""
+			n = 1
+			if nrof ~= nil then
+				n = tonumber(nrof)
+				if n < 1 then
+					n = 1
+				elseif n>MAX_NROF then
+					n = MAX_NROF
+				end
+			end
+			
+			-- pl:Write(n, game.COLOR_YELLOW)
+			
+			local ob = pl:CreateObject(for_sale[i].arch, game.IDENTIFIED, n) -- create 1 identified obj
 			if (ob~=nil) then
-				pl:PickUp(ob)
+				local l = 0
+				if level ~= nil then
+					l = tonumber(level)
+					if l < 0 then
+						l = 0
+					elseif l>MAX_LEVEL then
+						l = MAX_LEVEL
+					end
+				end	
+				ob.level = l
+				
+				-- calculation of price is value*(level+1)*n
+				-- do we need check value? if someone is adjusting bad values in archetyp/artifactst
+				if pl:PayAmount(ob.value*(ob.level+1)*n) == 1 then
+						pl:PickUp(ob,nil,n)
+				else
+					pl:Write("You don't have enough money.!", game.COLOR_YELLOW)
+				end			
 			end
 		end
+	end
+	topicDefault()
+end
+
+local function topicLevel(_level)
+	if _level ~= nil then
+		level = tonumber(_level)
+		pl:Write(level, game.COLOR_YELLOW)
+		ds:Set("level", level) -- calling topicDefault, restart the whole script, so we have check at script start for MIN and MAX
+		-- ds:WasChanged() -- looks like this is not needed
+	end
+	topicDefault()
+end
+
+local function topicNrof(_nrof)
+	if _nrof ~= nil then
+		nrof = tonumber(_nrof)
+		pl:Write(nrof, game.COLOR_YELLOW)
+		ds:Set("nrof", tonumber(nrof)) -- calling topicDefault, restart the whole script, so we have check at script start for MIN and MAX
+		-- ds:WasChanged() -- looks like this is not needed
 	end
 	topicDefault()
 end
@@ -70,5 +184,7 @@ end
 tl = TopicList()
 tl:AddGreeting(nil, topicDefault)
 tl:SetDefault(topicDefault)
-tl:AddTopics("buy (%d)", topicBuy) 
+tl:AddTopics("buy (%d) (%d) (%d)", topicBuy)
+tl:AddTopics("nrof (%d)", topicNrof)
+tl:AddTopics("level (%d)", topicLevel)
 tl:CheckMessage(event)
