bf466dd446e982fb0ba00d423c11e6f3befa3afc
Kamor <kamor1970@gmx.de>
2022-11-24 13:01:19 +0100

    Create find_traps.c
    changed skill to indirect skill (last_eat 1)

---
+++ arch/intern/skills/find_traps.c
@@ -0,0 +1,28 @@
+Object skill_find_traps
+name Find Traps
+is_named 1
+food 11
+run_away 0
+speed 0.0
+msg
+reveals nearby traps.
+It is used in a direction but has a range effect.
+The tile under the player and all around are searched.
+endmsg
+type 43
+item_quality 100
+item_condition 100
+nrof 1
+identified 1
+known_magical 1
+known_cursed 1
+subtype 1
+face skill.101
+inv_face sk_014.101
+sys_object 1
+last_eat 1
+no_drop 1
+sp 2
+level 1
+exp 0
+end
