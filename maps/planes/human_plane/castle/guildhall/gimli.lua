0db85d4db4eb2dd2c633b9c1e3c791d4fdc7b7c1
Kamor <kamor1970@gmx.de>
2022-11-30 16:35:00 +0100

    Update gimli.lua
    Compatiblity changes from pl to player and me to npc.
    Replaced service links/buttons to training.
    Cleaned the code formatting.

--- maps/planes/human_plane/castle/guildhall/gimli.lua
+++ maps/planes/human_plane/castle/guildhall/gimli.lua
@@ -1,5 +1,5 @@
-me = event.me  -- we need this global for included included npc_shop script
-pl = event.activator -- we need this global for included included npc_shop script
+npc = event.me  -- we need this global for npc_trainer
+player = event.activator -- we need this global for npc_trainer
 
 require("interface_builder")
 ib = InterfaceBuilder() -- we need this global ...
@@ -9,22 +9,22 @@ ib = InterfaceBuilder() -- we need this global ...
 -- we need this global ...
 to_train =
 {
-	{skill_name = "Cleave Weapons", icon = "axe_small.101"},
-	{skill_name = "Impact Weapons", icon = "club.101"},
-	{skill_name = "Pierce Weapons", icon = "dagger.101"},
-	{skill_name = "Slash Weapons", icon = "shortsword.101"},
-	{skill_name = "Throwing", icon = "t_dagger.101"}
+  {skill_name = "Cleave Weapons", icon = "axe_small.101"},
+  {skill_name = "Impact Weapons", icon = "club.101"},
+  {skill_name = "Pierce Weapons", icon = "dagger.101"},
+  {skill_name = "Slash Weapons", icon = "shortsword.101"},
+  {skill_name = "Throwing", icon = "t_dagger.101"}
 }
 
 local function topic_greeting()
   ib:SetTitle("Greetings")
   ib:SetMsg("Ey, you want some weapon training?")
-	ib:AddMsg("\n\nI know a lot about weapons.\n")
-	ib:AddMsg("\n\nI can train you to handle cleave, impact, pierce, slash weapons and can show you, how to become better in this skills.\n")
-	ib:AddMsg("\n\nI also know some basics about throwing weapon skills, if you need some training there.\n")
-	ib:AddLink("Sure.","Services")
-	ib:AddLink("No, not now.","")
-	ib:SetLHSButton("Services")
+  ib:AddMsg("\n\nI know a lot about weapons.\n")
+  ib:AddMsg("\n\nI can train you to handle cleave, impact, pierce, slash weapons and can show you, how to become better in this skills.\n")
+  ib:AddMsg("\n\nI also know some basics about throwing weapon skills, if you need some training there.\n")
+  ib:AddLink("Sure.","training") -- or "Training"
+  ib:AddLink("No, not now.","")
+  ib:SetLHSButton("Training","training") -- or ib:SetLHSButton("Training")
 end
 
 require("/scripts/npc_trainer.lua")
