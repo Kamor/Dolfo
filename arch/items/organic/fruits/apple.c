1ef47ed6c4ad529224b2e3167f3a41826bf47dd4
Kamor <kamor1970@gmx.de>
2022-12-01 17:45:04 +0100

    Create apple.c

---
+++ arch/items/organic/fruits/apple.c
@@ -0,0 +1,18 @@
+Object apple
+name apple
+face apple01.101
+last_eat 2
+hp 25
+sp 25
+can_stack 1
+grace 25
+nrof 1
+layer 3
+type 6
+speed_left 0.002
+material 32
+material_real -1
+value 10
+weight 400
+subtype 1
+end
